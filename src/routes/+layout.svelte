badbc9b68c4bccbfc9c299d52d1b3747682fb317
Jonathan Gamble <jdgamble555@gmail.com>
2025-01-19 21:07:44 -0600

    first

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { browser } from "$app/environment";
+  import "../app.css";
+  let { children } = $props();
+
+  const markup = `<div ${browser ? "light" : "dark"}></div>`;
+</script>
+
+{@render children()}
+
+<svelte:head>
+  <!-- svelte-ignore hydration_html_changed hydration_mismatch -->
+  {@html markup}
+</svelte:head>
