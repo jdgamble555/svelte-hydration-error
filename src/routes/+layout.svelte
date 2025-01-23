62b5278c2f00118507569a3d2badd506c6b9d371
Jonathan Gamble <jdgamble555@gmail.com>
2025-01-23 13:47:32 -0600

    same line

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,11 @@
   import { browser } from "$app/environment";
   import "../app.css";
   let { children } = $props();
-
 </script>
 
 {@render children()}
 
 <svelte:head>
-  <!-- svelte-ignore hydration_html_changed -->
-  <!-- eslint-disable-next-line svelte/no-at-html-tags, prefer-template -->
-  {@html `<script> ${browser ? "\nconst light = true\;" : "\nconst dark = true\;"}</script>`}
+  <!-- eslint-disable-next-line svelte/no-at-html-tags, prefer-template--><!-- svelte-ignore hydration_html_changed -->
+  {@html `<script> ${browser ? "\nconst light = true;" : "\nconst dark = true;"}</script>`}
 </svelte:head>
